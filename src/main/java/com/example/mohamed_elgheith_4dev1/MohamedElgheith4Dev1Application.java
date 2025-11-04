3c48876053d2334be7fd954a4a4645f5dae1dd15
Cheikhtourad19 <elghothvadel@gmail.com>
2025-11-04 10:35:09 +0100

    Initial commit

---
+++ src/main/java/com/example/mohamed_elgheith_4dev1/MohamedElgheith4Dev1Application.java
@@ -0,0 +1,13 @@
+package com.example.mohamed_elgheith_4dev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MohamedElgheith4Dev1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MohamedElgheith4Dev1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mohamed_elgheith_4dev1/MohamedElgheith4Dev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mohamed_elgheith_4dev1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MohamedElgheith4Dev1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
